d1a5456e49db53e2424ca298acd9b695988834c1
emi <noemie.pascalin@eduvaud.ch>
2022-03-29 10:08:54 +0200

    hover change article 2

--- article-2/article-2.js
+++ article-2/article-2.js
@@ -1,13 +1,3 @@
-import {
-  wrapParagraphWords,
-  selectWordElements,
-  createDraggableCensor,
-} from "../components/ParagraphCensors";
-
 import "../css/reset.css";
 import "../css/style.css";
 import "./article-2.css";
-
-wrapParagraphWords();
-const selectedWordElement = selectWordElements(100);
-createDraggableCensor(selectedWordElement);
